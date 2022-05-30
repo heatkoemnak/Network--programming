32c6218fc52836fca8d5294fa9bbf1fb524dc3f9
koemnak <heatkimnak@gmail.com>
2022-05-30 15:32:22 +0700

    thirt version

---
+++ search3.py
@@ -0,0 +1,34 @@
+# Use/bin/env Python3
+# The above line is a shebang line defines where the interpreter is located.
+# Network Programming
+# Week01 search4.py
+
+# (Nominatim Openstreetmap.org APT)
+
+import socket
+import ssl
+from urllib.parse import quote_plus
+request_text ="""\
+GET /search?q={}&format=json HTTP/1.1\r\n\
+Host: nominatiom.openstreetmap.org\r\n\
+User-Agent: Network Programming example search4.py\r\n\
+Connection: close\r\n
+\r\n
+"""
+
+def geocode(address):
+    unencrypted_sock = socket.socket()
+    unencrypted_sock.connect(('nominatim.openstreetmap.org',443))
+    sock = ssl.wrap_socket(unencrypted_sock)
+    request = request_text.format(quote_plus(address))
+    sock.sendall(request.encode('ascii'))
+    raw_reply = b''
+    while True:
+        more =sock.recv(4096)
+        if not more:
+            break
+        raw_reply += more
+        print(raw_reply.decode('utf-8'))
+
+    if __name__== '__main__':
+        geocode('វិទ្យាល៍យព្រះយុគគន្ធរ')
